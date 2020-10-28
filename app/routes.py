5f0ebc688f3445c8b4d118a8de0f2972ec36706a
Arjun Balamwar <arjunbalamwar@gmail.com>
2020-10-28 16:40:37 +0530

    Dummy data

--- app/routes.py
+++ app/routes.py
@@ -30,12 +30,17 @@ def register():
 		return redirect(url_for('home'))
 	form=RegistrationForm()
 	if form.validate_on_submit():
-		hashed_password= bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		user=User(username=form.username.data, email=form.email.data, password=hashed_password)
-		db.session.add(user)
-		db.session.commit()
-		flash('Your Account has been created. You will be able to Login Now !!','success')
-		return redirect(url_for('login'))
+		if form.password.data != form.confirm_password.data:
+			print("=============================================CHECKED====================================")
+			flash("Passwords do not match", 'danger')
+		else:
+			hashed_password= bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+			user=User(username=form.username.data, email=form.email.data, password=hashed_password)
+			db.session.add(user)
+			db.session.commit()
+			flash('Your Account has been created. You will be able to Login Now !!','success')
+			return redirect(url_for('login'))
+			
 	return render_template('register.html', title='Register Page' ,form=form)
 
 @app.route('/login', methods=['GET','POST'])
